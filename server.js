2c9513ac4e0e45e12027b36bb1fd7fc18f7d3358
Luiz Felipe <luizfelipesoarees@gmail.com>
2024-06-02 19:26:42 -0300

    Update 1.3

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = 3000;
-const JWT_SECRET = 'seu_segredo_para_token'; // Altere para um segredo mais seguro
+const JWT_SECRET = 'segredo_token'; // Altere para um segredo mais seguro
 
 // Configuração do banco de dados
 const config = {
-    user: 'Luiz',
-    password: 'LinkZ0903#',
-    server: 'fatec-projetos.database.windows.net',
-    database: 'fatec-projeto1',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     options: {
         encrypt: true // Dependendo da configuração do seu servidor SQL Server
     }
